76b82b9f00a5b9b9e45b98141573f602154a849c
Nambu Kamali <nambukamali@gmail.com>
2026-03-03 21:02:17 +0530

    Add files via upload

---
+++ Candidate.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADO.NET_APP.Models
+{
+    public class Candidate
+    {
+        [Key]
+        public int CandidateID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Stack { get; set; } = string.Empty;
+        public string Education { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
