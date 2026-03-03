ea2bd515bbad0c9ea8a9fcf88f5ef83da27652d5
Nambu Kamali <nambukamali@gmail.com>
2026-03-03 21:02:50 +0530

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ADO.NET_APP.Views.Candidate
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
