a130539a842f1500ff1365c129c7eae61547fee3
Nambu Kamali <nambukamali@gmail.com>
2026-03-03 21:01:18 +0530

    Add files via upload

---
+++ CandidateController.cs
@@ -0,0 +1,27 @@
+﻿using ADO.NET_APP.DataAccess;
+using ADO.NET_APP.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace ADO.NET_APP.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private readonly CandidateService _candidateService;
+        public CandidateController()
+        {
+            _candidateService = new CandidateService();
+        }
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            Candidate candidate = _candidateService.GetCandidate(id);
+            if (candidate == null)
+                return NotFound();
+            return View(candidate);
+        }
+
+    }
+}
